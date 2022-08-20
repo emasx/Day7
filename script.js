c789a661b62a3214fdc77832301cf8d020ac8a42
emasx <29961897+emasx@users.noreply.github.com>
2022-08-20 23:31:26 +0300

    if/else

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+const age = 15;
+
+
+if(age >= 18) {
+    console.log("Sarah can start driving license.");
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years.`);
+}
+
+
+
+
+
+const birthYear = 2012;
+let century;
+
+if(birthYear <= 2000) {
+     century = 20;
+} else {
+     century = 21;
+}
+
+console.log(century);
+
